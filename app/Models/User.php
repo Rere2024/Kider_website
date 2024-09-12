1546cf66bf2562331f88449838ac6a4152dcfe73
Rere2024 <www.rehabmonir2024@gmail.com>
2024-09-12 23:35:23 +0300

    kidr9

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'//5
     ];
 
     /**
